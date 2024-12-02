5167ddb5e57c4d16514f3f849c338a3d257beb0c
Romiyooo <romeotomtom12@gmail.com>
2024-12-02 15:27:32 +0800

    Donation Form Complete.

--- GoodSamariTAN/settings.py
+++ GoodSamariTAN/settings.py
@@ -11,6 +11,7 @@ https://docs.djangoproject.com/en/5.1/ref/settings/
 """
 
 from pathlib import Path
+import os
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -54,7 +55,7 @@ ROOT_URLCONF = 'GoodSamariTAN.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [],
+        'DIRS': [os.path.join(BASE_DIR,'application/templates')],
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
@@ -120,4 +121,19 @@ STATIC_URL = 'static/'
 # Default primary key field type
 # https://docs.djangoproject.com/en/5.1/ref/settings/#default-auto-field
 
+INSTALLED_APPS = [
+    # Other apps
+    'django.contrib.admin',
+    'django.contrib.auth',
+    'django.contrib.contenttypes',
+    'django.contrib.sessions',
+    'django.contrib.messages',
+    'django.contrib.staticfiles',
+    # Your app
+    'application',
+]
+
+
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+MEDIA_URL = '/media/'
+MEDIA_ROOT = BASE_DIR / 'media'
--- application/models.py
+++ application/models.py
@@ -1,3 +1,19 @@
+# models.py
 from django.db import models
 
-# Create your models here.
+class DonationForm(models.Model):
+    name = models.CharField(max_length=100)
+    phone = models.CharField(max_length=15)
+    email = models.EmailField()
+    item = models.CharField(max_length=100)
+    quantity = models.IntegerField()
+    category = models.CharField(max_length=50)
+    description = models.TextField()
+    height = models.IntegerField()  
+    width = models.IntegerField()    
+    weight = models.IntegerField()  
+    notes = models.TextField(null=True, blank=True)
+    image = models.ImageField(upload_to='donations/', null=True, blank=True)
+
+    def __str__(self):
+        return self.name
